7a85c759a21f6e21686c7c068f2e61ca8cd9427f
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2025-07-10 15:03:38 +0530

    zappy

--- init/index.js
+++ init/index.js
@@ -3,28 +3,26 @@ const User = require("../models/user");
 const initUserData = require("./userData");
 require("dotenv").config();
 
-main()
-.then(()=>{
-    console.log("connected to mongodb successfully!");
-}).catch((err)=>{
-    console.log("failed to connect with mongodb!");
-});
-
 async function main() {
+  try {
     await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log("Connected to MongoDB successfully!");
+
+    await initDB();
+
+    console.log("User data successfully inserted into database!");
+  } catch (err) {
+    console.error("Failed to connect or insert data:", err);
+    process.exit(1);
+  }
 }
 
 async function initDB() {
-    await User.deleteMany({});
-    await User.insertMany(initUserData.data);
+  await User.deleteMany({});
+  await User.insertMany(initUserData.data);
 }
 
-initDB()
-.then(()=>{
-    console.log("user data successfully inserted into database!");
-}).catch((err)=>{
-    console.log("failed to insert user data", err);
-})
+main();
