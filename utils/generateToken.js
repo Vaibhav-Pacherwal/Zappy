9b258d13369f3773d058bb1fcc8a96174295784c
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2025-07-13 16:17:57 +0530

    zappy

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const generateToken = (userId)=>{
     return jwt.sign({id: userId}, 
         process.env.JWT_SECRET, 
-        {expiresIn: process.env.JWT_EXPIRES_IN});
+        {expiresIn: process.env.JWT_EXPIRES_IN || '30d'});
 }
 
 module.exports = generateToken;
